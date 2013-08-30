c7b028633e333014ad1c97b7baa828fcffd88e07
Stephan Jorek <stephan.jorek@gmail.com>
2013-08-30 20:15:23 +0200

    Generated documentation for 0ca027c01062b01ea72ddcefa966ac20018bab66

--- assets/behavior.js
+++ assets/behavior.js
@@ -1433,16 +1433,8 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
                   "type": "heading",
                   "data": {
                     "level": 2,
-                    "title": "CSS Properties",
+                    "title": "CSS properties",
                     "slug": "css-properties"
-                  },
-                  "depth": 2
-                }, {
-                  "type": "heading",
-                  "data": {
-                    "level": 2,
-                    "title": "Typeof strings",
-                    "slug": "typeof-strings"
                   },
                   "depth": 2
                 }, {
